27d9da373df6c39ad0dc3eed9d580c84a40871ff
ahmedazagaz <contactwithazagaz@gmail.com>
2024-01-23 02:35:57 -0800

    Updated header file

--- sort.h
+++ sort.h
@@ -1,9 +1,10 @@
-#ifndef _SORT_H
-#define _SORT_H
+#ifndef SORT_H
+#define SORT_H
 
+
+#include <stddef.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
+
 
 /**
  * struct listint_s - Doubly linked list node
@@ -19,20 +20,28 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* print */
-void bubble_sort(int *array, size_t size);
-void swap(listint_t *x, listint_t *y)
+/* Print Functions */
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
+
+/* Sorting Algorithms Functions */
+
+void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-
 void selection_sort(int *array, size_t size);
-
 void quick_sort(int *array, size_t size);
-void qs(int *array, size_t size, ssize_t small, ssize_t high);
-size_t lomuto_partition(int *array, size_t size, ssize_t small, ssize_t high);
-void swap(int *array, size_t size, int *n1, int *n2);
 
-#endif
+
+/*Advanced Tasks*/
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
+#endif /* SORT_H */
